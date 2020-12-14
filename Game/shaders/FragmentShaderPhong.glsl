a6478d3dd1467335d4002da0f0fae536e16543d0
magalenyo <magalenyo7@gmail.com>
2020-12-14 01:03:50 +0100

    Added phong shader

--- Game/shaders/FragmentShaderPhong.glsl
+++ Game/shaders/FragmentShaderPhong.glsl
@@ -1,8 +1,8 @@
 #version 330
 
-//in vec4 diffuseColor;
 in vec3 surface_normal;
 in vec3 surface_position;
+in vec2 uv0;
 
 uniform vec3 light_dir;
 uniform vec3 view_pos;
@@ -11,29 +11,28 @@ uniform float Ks;
 uniform float Kd;
 uniform float shininess;
 uniform vec3 light_color;
-uniform vec3 diffuse_color;
+
+uniform sampler2D diffuse_texture;
 
 out vec4 outColor;
 
 void main() {
+
+	vec3 diffuse_color = texture2D(diffuse_texture, uv0).xyz;
 	vec3 nlight_dir = normalize(light_dir);
-	float NL = max(dot(normalize(surface_normal), nlight_dir), 0.0);		// diffuse
+	float NL = max(dot(normalize(surface_normal), nlight_dir), 0.0);
 	
 	vec3 ambient = ambient_color * diffuse_color;
 	if (NL > 0) {	// if in front of light
 		vec3 R = reflect(nlight_dir, normalize(surface_normal));
 		vec3 V = normalize(view_pos - surface_position);
 		float VR = max(dot(V, R), 0.0);
-		float specular = pow(VR, shininess);		// specular
+		vec3 specular = Ks * light_color * pow(VR, shininess);
+		vec3 diffuse = Kd * diffuse_color * light_color * NL;
 		
-		
-		outColor = vec4(ambient_color * diffuse_color + 
-					    Kd * diffuse_color * light_color * NL + 
-					    Ks * light_color * specular, 1.0);
+		outColor = normalize(vec4(ambient + diffuse + specular, 1.0));
 	}
 	else{	// is black
-		outColor = vec4(ambient, 1.0);
+		outColor = normalize(vec4(ambient, 1.0));
 	}
-
-	 
 }
--- Game/shaders/VertexShaderPhong.glsl
+++ Game/shaders/VertexShaderPhong.glsl
@@ -3,16 +3,17 @@ layout (location = 0) in vec3 vertex_position;
 layout (location = 1) in vec3 vertex_normal;
 layout (location = 1) in vec2 vertex_uv0;
 
-uniform mat4 matVP;
+uniform mat4 proj;
+uniform mat4 view;
 uniform mat4 model;
 
 out vec3 surface_normal;
 out vec3 surface_position;
-out vec2 uv;
+out vec2 uv0;
 
 void main() {
-   gl_Position 		= matVP*model*vec4(vertex_position, 1.0);
+   gl_Position 		= proj*view*model*vec4(vertex_position, 1.0);
    surface_position = (model*vec4(vertex_position, 1.0)).xyz;
    surface_normal	= transpose(inverse(mat3(model)))*vertex_normal;
-   uv = vertex_uv0;
+   uv0 = vertex_uv0;
 }
