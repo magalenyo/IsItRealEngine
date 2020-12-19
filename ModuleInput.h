5b6ea217c9089da2ca5c3dffa8b7241f2d9b1e8c
magalenyo <magalenyo7@gmail.com>
2020-12-19 12:56:33 +0100

    @isaacfdez merge Camera and Input Modules

--- ModuleInput.h
+++ ModuleInput.h
@@ -2,12 +2,14 @@
 #include "Module.h"
 #include "Point.h"
 #include "Globals.h"
+#include "Math/float2.h"
 
 #include "SDL/include/SDL_scancode.h"
 
 typedef unsigned __int8 Uint8;
 
 #define NUM_MOUSE_BUTTONS 5
+#define MAX_KEYS 300
 
 enum KeyState
 {
@@ -30,13 +32,6 @@ public:
 		WE_RESIZED = 3,
 		WE_COUNT
 	};
-
-	enum MouseWheelState
-	{
-		SCROLLING_UP,
-		SCROLLING_DOWN,
-		SCROLLING_IDLE
-	};
 	
 	ModuleInput();
 	~ModuleInput();
@@ -44,35 +39,20 @@ public:
 	bool Init();
 	update_status Update();
 	bool CleanUp();
-	// Check key states (includes mouse and joy buttons)
-	KeyState GetKey(int id) const
-	{
-		return keyboard[id];
-	}
 
-	KeyState GetMouseButtonDown(int id) const
-	{
-		return mouse_buttons[id - 1];
-	}
-
-	// Check for window events last frame
-	bool GetWindowEvent(EventWindow ev) const
-	{
-		return windowEvents[ev];
-	}
-
-	MouseWheelState GetMouseWheelState() const
-	{
-		return mouseWheelState;
-	}
+	KeyState GetKey(int id) const { return keyboard[id]; }
+	KeyState GetMouseButtonDown(int id) const { return mouseButtons[id - 1]; }
+	bool GetWindowEvent(EventWindow ev) const { return windowEvents[ev]; }
+	const float GetMouseWheelMotion() const { return mouseWheelMotion; }
+	const float2& GetMouseMotion() const { return mouseMotion; }
 
 private:
 	//const Uint8 *keyboard = NULL;
 	bool		windowEvents[WE_COUNT];
-	KeyState*   keyboard;
-	KeyState	mouse_buttons[NUM_MOUSE_BUTTONS];
+	KeyState	keyboard[MAX_KEYS];
+	KeyState	mouseButtons[NUM_MOUSE_BUTTONS];
 
-	MouseWheelState	mouseWheelState = MouseWheelState::SCROLLING_IDLE;
-	iPoint mouse_motion;
-	iPoint mouse;
+	float2 mouseMotion = { 0, 0 };
+	float2 mouse = { 0, 0 };
+	float mouseWheelMotion = 0;
 };
